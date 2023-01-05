f11dc4c75e9bd0de999664a82f67cc733cd611a7
Muhammad Muso <musogulomov123@gmail.com>
2023-01-05 12:05:52 +0500

    INFRA: Initialize Core Api Project

---
+++ ToDoList.Core/ToDoList.Core.Api/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace ToDoList.Core.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() =>
+            Ok("Hello!");
+    }
+}
