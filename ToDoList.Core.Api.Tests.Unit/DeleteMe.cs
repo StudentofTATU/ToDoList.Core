1599dc821e0940f345e50b102890b455e7f6715a
Muhammad Muso <musogulomov123@gmail.com>
2023-01-05 15:41:42 +0500

    INFRA: Initialize Unit Test Project

---
+++ ToDoList.Core.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,15 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using Xunit;
+
+namespace ToDoList.Core.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}
--- ToDoList.Core.Api/Controllers/HomeController.cs
+++ ToDoList.Core.Api/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 // Copyright (c) Coalition of Good-Hearted Engineers
 // Free to use to bring order in your workplace
 //=================================
+
 using Microsoft.AspNetCore.Mvc;
 using RESTFulSense.Controllers;
 
