cc13a74e987d35cace211ed6fac0894619bf6a16
Muhammad Muso <musogulomov123@gmail.com>
2023-01-10 14:10:28 +0500

    INFRA: Cleanup

--- ToDoList.Core.Api/Program.cs
+++ ToDoList.Core.Api/Program.cs
@@ -1,37 +1,20 @@
-using Microsoft.AspNetCore.OpenApi;
+//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
 
 namespace ToDoList.Core.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args) =>
+            CreateHostBuilder(args).Build().Run();
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                    webBuilder.UseStartup<Startup>());
         }
     }
 }
