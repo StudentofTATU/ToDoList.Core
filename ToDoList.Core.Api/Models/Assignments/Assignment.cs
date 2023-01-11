b69db93a249cda5a31cea8994b40a079793397b4
Muhammad Muso <musogulomov123@gmail.com>
2023-01-11 21:44:52 +0500

    DATA: Model Assignment

---
+++ ToDoList.Core.Api/Models/Assignments/Assignment.cs
@@ -0,0 +1,20 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using Tarteeb.Api.Models.Tickets;
+
+namespace ToDoList.Core.Api.Models.Assignments
+{
+    public class Assignment
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Priority Priority { get; set; }
+        public AssignmentStatus AssignmentStatus { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
+    }
+}
---
+++ ToDoList.Core.Api/Models/Assignments/Priority.cs
@@ -0,0 +1,15 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+namespace ToDoList.Core.Api.Models.Assignments
+{
+    public enum Priority
+    {
+        UNKNOWN,
+        URGENT,
+        IMPORTANT,
+        MINOR,
+    }
+}
