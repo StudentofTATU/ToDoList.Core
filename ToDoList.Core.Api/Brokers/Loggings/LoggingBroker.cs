a3d058e74e504595c9f8b33501c14b6faea0d63a
Muhammad Muso <musogulomov123@gmail.com>
2023-01-16 21:20:17 +0500

    BROKERS: Add Logging

---
+++ ToDoList.Core.Api/Brokers/Loggings/LoggingBroker.cs
@@ -0,0 +1,20 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+namespace ToDoList.Core.Api.Brokers.Loggings
+{
+    public class LoggingBroker : ILoggingBroker
+    {
+        private readonly ILogger<LoggingBroker> logger;
+        public LoggingBroker(ILogger<LoggingBroker> logger) =>
+            this.logger = logger;
+
+        void ILoggingBroker.LogError(Exception exception) =>
+            this.logger.LogError(exception, exception.Message);
+
+        void ILoggingBroker.LogCritical(Exception exception) =>
+            this.logger.LogCritical(exception.Message, exception);
+    }
+}
