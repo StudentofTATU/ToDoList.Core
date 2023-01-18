9b8eaab6480ea0f1f17e5a3850fd7482dd173418
Muhammad Muso <musogulomov123@gmail.com>
2023-01-18 11:51:08 +0500

    ShouldThrowValidationExceptionOnAddIfAssignmentIsInvalidAndLogItAsync-> FAIL

--- ToDoList.Core.Api.Tests.Unit/Services/Foundations/Assignments/AssignmentServiceTests.Validations.Add.cs
+++ ToDoList.Core.Api.Tests.Unit/Services/Foundations/Assignments/AssignmentServiceTests.Validations.Add.cs
@@ -42,5 +42,64 @@ namespace ToDoList.Core.Api.Tests.Unit.Services.Foundations.Assignments
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfAssignmentIsInvalidAndLogItAsync(
+            string invalidString)
+        {
+            // given
+            var invalidAssignment = new Assignment
+            {
+                Title = invalidString
+            };
+
+            var invalidAssingmentException = new InvalidAssingmentException();
+
+            invalidAssingmentException.AddData(
+                key: nameof(Assignment.Id),
+                values: "Id is required");
+
+            invalidAssingmentException.AddData(
+                key: nameof(Assignment.Title),
+                values: "Text is required");
+
+            invalidAssingmentException.AddData(
+               key: nameof(Assignment.Description),
+               values: "Text is required");
+
+            invalidAssingmentException.AddData(
+               key: nameof(Assignment.CreatedDate),
+               values: "Value is required");
+
+            invalidAssingmentException.AddData(
+                key: nameof(Assignment.UpdatedDate),
+                values: "Value is required");
+
+            AssignmentValidationException expectedAssignmentValidationException =
+                new AssignmentValidationException(invalidAssingmentException);
+
+            // when
+            ValueTask<Assignment> addAssginmentTask =
+                this.assignmentService.AddAssignmentAsync(invalidAssignment);
+
+            AssignmentValidationException actualAssignmentValidationException =
+                await Assert.ThrowsAsync<AssignmentValidationException>(addAssginmentTask.AsTask);
+
+            // then
+            actualAssignmentValidationException.Should().BeEquivalentTo(expectedAssignmentValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExpressionAs(
+                    expectedAssignmentValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertAssignmentAsync(It.IsAny<Assignment>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
