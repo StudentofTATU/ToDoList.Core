73486103da8b433527f85b2e2db0f228fc7b2eaf
Muhammad Muso <musogulomov123@gmail.com>
2023-01-20 21:49:41 +0500

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursAndLogItAsync-> PASS

--- ToDoList.Core.Api.Tests.Unit/Services/Foundations/Assignments/AssignmentServiceTests.Exceptions.Add.cs
+++ ToDoList.Core.Api.Tests.Unit/Services/Foundations/Assignments/AssignmentServiceTests.Exceptions.Add.cs
@@ -29,8 +29,8 @@ namespace ToDoList.Core.Api.Tests.Unit.Services.Foundations.Assignments
             var expectedAssignmentDependencyException =
                 new AssignmentDependencyException(failedAssignmentStorageException);
 
-            this.storageBrokerMock.Setup(broker =>
-                broker.InsertAssignmentAsync(It.IsAny<Assignment>())).ThrowsAsync(sqlException);
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTime()).Throws(sqlException);
 
             // when
             ValueTask<Assignment> addAssigmentTask =
@@ -42,15 +42,19 @@ namespace ToDoList.Core.Api.Tests.Unit.Services.Foundations.Assignments
             // then
             actualAssignmentDependencyException.Should().BeEquivalentTo(expectedAssignmentDependencyException);
 
-            this.storageBrokerMock.Verify(broker =>
-                broker.InsertAssignmentAsync(It.IsAny<Assignment>()), Times.Once);
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(), Times.Once);
 
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCritical(It.Is(SameExpressionAs(
                     expectedAssignmentDependencyException))), Times.Once);
 
-            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertAssignmentAsync(It.IsAny<Assignment>()), Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
         }
 
         [Fact]
