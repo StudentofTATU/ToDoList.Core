a0cb6450265307a108d5f8914bc27e7122ff70f7
Muhammad Muso <musogulomov123@gmail.com>
2023-02-05 11:12:18 +0500

    ShouldThrowDependencyValidationOnRemoveIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync-> FAIL

---
+++ ToDoList.Core.Api.Tests.Unit/Services/Foundations/Assignments/AssignmentServiceTests.Exceptions.RemoveById.cs
@@ -0,0 +1,61 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using ToDoList.Core.Api.Models.Assignments;
+using ToDoList.Core.Api.Models.Assignments.Exceptions;
+using Xunit;
+
+namespace ToDoList.Core.Api.Tests.Unit.Services.Foundations.Assignments
+{
+    public partial class AssignmentServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowDependencyValidationOnRemoveIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someAssignmentId = Guid.NewGuid();
+
+            var databaseUpdateConcurrencyException = new DbUpdateConcurrencyException();
+
+            var lockedAssignmentException =
+                new LockedAssignmentException(databaseUpdateConcurrencyException);
+
+            var expectedAssignmentDependencyValidationException =
+                new AssignmentDependencyValidationException(lockedAssignmentException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAssignmentByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(databaseUpdateConcurrencyException);
+
+            // when
+            ValueTask<Assignment> removeAssignmentByIdTask =
+                this.assignmentService.RemoveAssignmentByIdAsync(someAssignmentId);
+
+            AssignmentDependencyValidationException actualAssignmentDependencyValidationException =
+                await Assert.ThrowsAsync<AssignmentDependencyValidationException>(
+                    removeAssignmentByIdTask.AsTask);
+
+            // then
+            actualAssignmentDependencyValidationException.Should().BeEquivalentTo(
+                expectedAssignmentDependencyValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAssignmentByIdAsync(It.IsAny<Guid>()), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExpressionAs(
+                    expectedAssignmentDependencyValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteAssignmentAsync(It.IsAny<Assignment>()), Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
